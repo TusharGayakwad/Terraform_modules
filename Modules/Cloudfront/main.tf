c5aaf257c401427ecc86e7b48d0d0052a49ffc5f
Tushar Gayakwad <tushargayakwad29@gmail.com>
2025-01-15 00:09:41 +0530

    add a cloudfront module.

---
+++ Modules/Cloudfront/main.tf
@@ -0,0 +1,55 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = var.bucket_regional_domain_name
+    # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = var.s3_origin_id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.s3_origin_id
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "mylogs.s3.amazonaws.com"
+    prefix          = "myprefix"
+  }
+
+  aliases = ["mysite.example.com", "yoursite.example.com"]
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
---
+++ Modules/Cloudfront/varible.tf
@@ -0,0 +1,10 @@
+variable "bucket_regional_domain_name" {
+  type = string
+  description = "subnet_id"
+}
+
+
+variable "s3_origin_id" {
+  type = string
+  description = "subnet_id"
+}
